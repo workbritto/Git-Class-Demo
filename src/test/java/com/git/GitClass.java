b2fb911b3e8de0dd615c20a1f4ef7920fc847b80
workbritto <workbritto@gmail.com>
2022-02-21 20:29:14 +0530

    Inital code

---
+++ src/test/java/com/git/GitClass.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class GitClass {
+	private void method1() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("2");
+	}
+	private void method3() {
+		System.out.println("3");
+	}
+	private void method4() {
+		System.out.println("4");
+	}
+	private void method5() {
+		System.out.println("5");
+	}
+}
