46984aecb0ea08bd1da8f900e73fae164a429837
workbritto <workbritto@gmail.com>
2022-02-22 19:56:06 +0530

    upgraded

--- src/test/java/com/git/GitClass.java
+++ src/test/java/com/git/GitClass.java
@@ -19,10 +19,4 @@ public class GitClass {
 	private void method6() {
 		System.out.println("6");
 	}
-	private void method7() {
-		System.out.println("7");
-	}
-	private void method8() {
-		System.out.println("8");
-	}
 }
